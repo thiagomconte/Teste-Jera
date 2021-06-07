e8bffdf4c6efb718123278dae27a5a5916ef9b90
thiagomconte <thiagomconte@hotmail.com>
2021-06-06 21:01:02 -0300

    first version deploy

---
+++ dist/precache-manifest.3e3e1ea45a9fb5c0c50a3a05f93e0729.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e189765d9c5dce19ca65",
+    "url": "/css/app.107da5d6.css"
+  },
+  {
+    "revision": "4f8f25dc95cc5693936f",
+    "url": "/css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "5390a04c43bce93cb43597c4b98e422e",
+    "url": "/img/jeraflix.5390a04c.png"
+  },
+  {
+    "revision": "e9fc09c7fef772e6d8b644a83391bdd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e189765d9c5dce19ca65",
+    "url": "/js/app.27a7616f.js"
+  },
+  {
+    "revision": "4f8f25dc95cc5693936f",
+    "url": "/js/chunk-vendors.d387ba30.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
